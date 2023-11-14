8b0fdc772eec2436d660b3ce9235c7036879c445
ViswaDhanushReddy <Viswa234587@gmail.com>
2023-11-15 00:52:12 +0530

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormsModule}from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EXP5';
+  list:any[]=[];
+  addtask(item:string)
+  {
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list);
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id);
+  }
 }
